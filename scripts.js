e149e2328435b9a8cc087a301eb63624a52af0f4
haribduvihan <harinduvihan@gmail.com>
2025-04-11 22:32:05 +0530

    updated js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,20 @@
 var sideMenu = document.getElementById("sideMenu");
+var menuIcon = document.getElementById("menuIcon");
 
 function openMenu() {
-  sideMenu.style.opacity = "1";
-  sideMenu.style.width = "100%";
+  sideMenu.style.width = "150px";
   sideMenu.style.height = "100%";
+
+  menuIcon.style.opacity = "0";
+  menuIcon.style.width = "0px";
+  menuIcon.style.height = "0px";
 }
 
 function closeMenu() {
-  sideMenu.style.opacity = "0";
   sideMenu.style.width = "0px";
   sideMenu.style.height = "0px";
+
+  menuIcon.style.opacity = "1";
+  menuIcon.style.width = "25px";
+  menuIcon.style.height = "25px";
 }
