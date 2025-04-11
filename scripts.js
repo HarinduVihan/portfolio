32399db57310f871935df3d2aa4cd5f6529da705
haribduvihan <harinduvihan@gmail.com>
2025-04-11 14:23:42 +0530

    nav-bar almost done. in res

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var sideMenu = document.getElementById("sideMenu");
+
+function openMenu() {
+  sideMenu.style.opacity = "1";
+  sideMenu.style.width = "100%";
+  sideMenu.style.height = "100%";
+}
+
+function closeMenu() {
+  sideMenu.style.opacity = "0";
+  sideMenu.style.width = "0px";
+  sideMenu.style.height = "0px";
+}
